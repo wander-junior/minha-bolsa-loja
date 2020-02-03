566661edd6eb94d7145de822055c0bc5db0402c0
Wander <wanderjrcruz96@gmail.com>
2020-02-03 18:32:55 -0300

    Adiciona função de trocar imagens em produto.html

--- js/script.js
+++ js/script.js
@@ -6,9 +6,27 @@ new SimpleSlide({
     auto: true, // se o slide deve passar automaticamente
     time: 5000, // tempo de transição dos slides
     pauseOnHover: true // pausa a transição automática
-  });
+  })
 
 // Input Masks
 
-VMasker(document.querySelector(".cep")).maskPattern("99.999-999");
-VMasker(document.querySelector(".cpf")).maskPattern("999.999.999-99");
+VMasker(document.querySelector(".cep")).maskPattern("99.999-999")
+VMasker(document.querySelector(".cpf")).maskPattern("999.999.999-99")
+
+// Troca de imagens em produtos
+
+function trocaImagem(){
+  icone = document.getElementById("icone1")
+  principal = document.getElementsByClassName("imagem-principal")
+  tmp = principal[0].innerHTML
+  principal[0].innerHTML = icone.innerHTML
+  icone.innerHTML = tmp
+}
+
+function trocaImagem2(){
+  icone = document.getElementById("icone2")
+  principal = document.getElementsByClassName("imagem-principal")
+  tmp = principal[0].innerHTML
+  principal[0].innerHTML = icone.innerHTML
+  icone.innerHTML = tmp
+}
